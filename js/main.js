aaa2381c138f24537ce9da3bfd9a76eef053cf73
Mahmoud-Qasem0 <165132096+Mahmoud-Qasem0@users.noreply.github.com>
2024-09-23 18:57:58 +0300

    Upload design files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Scroll To Top Button
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+
+
+// Date updating in Footer
+let year = document.getElementById("year");
+
+year.innerHTML = new Date().getFullYear();
